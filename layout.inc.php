5e3811ff4d7fa7242d9a59f3063f808b8c0bb691
parkjongheum <parkjh0821@gmail.con>
2017-10-03 15:00:19 +0900

    initial commit

---
+++ layout.inc.php
@@ -0,0 +1,80 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * configures general layout
+ * for detailed layout configuration please refer to the css files
+ *
+ * @package    PhpMyAdmin-theme
+ * @subpackage PMAHomme
+ */
+
+/**
+ * navi frame
+ */
+// navi frame width
+$GLOBALS['cfg']['NaviWidth']                = 240;
+
+// foreground (text) color for the navi frame
+$GLOBALS['cfg']['NaviColor']                = 'rgba(0,0,0,0.87)';
+
+// background for the navi frame
+// .tweak-categories
+$GLOBALS['cfg']['NaviBackground']           = '#FAFAFA';
+
+// foreground (text) color of the pointer in navi frame
+// row:selected label
+$GLOBALS['cfg']['NaviPointerColor']         = '#FFFFFF';
+
+// background of the pointer in navi frame
+// row:selected
+$GLOBALS['cfg']['NaviPointerBackground']    = '#42A5F5';
+
+/**
+ * main frame
+ */
+// foreground (text) color for the main frame
+$GLOBALS['cfg']['MainColor']                = '#444';
+
+// background for the main frame
+$GLOBALS['cfg']['MainBackground']           = '#fff';
+
+// foreground (text) color of the pointer in browse mode
+$GLOBALS['cfg']['BrowsePointerColor']       = '#000';
+
+// background of the pointer in browse mode
+$GLOBALS['cfg']['BrowsePointerBackground']  = '#cfc';
+
+// foreground (text) color of the marker (visually marks row by clicking on it)
+// in browse mode
+$GLOBALS['cfg']['BrowseMarkerColor']        = '#000';
+
+// background of the marker (visually marks row by clicking on it) in browse mode
+$GLOBALS['cfg']['BrowseMarkerBackground']   = '#fc9';
+
+/**
+ * fonts
+ */
+/**
+ * the font family as a valid css font family value,
+ * if not set the browser default will be used
+ * (depending on browser, DTD and system settings)
+ */
+$GLOBALS['cfg']['FontFamily']           = 'sans-serif';
+/**
+ * fixed width font family, used in textarea
+ */
+$GLOBALS['cfg']['FontFamilyFixed']      = 'monospace';
+
+/**
+ * tables
+ */
+// border
+$GLOBALS['cfg']['Border']               = 0;
+// table header and footer color
+$GLOBALS['cfg']['ThBackground']         = '#D3DCE3';
+// table header and footer background
+$GLOBALS['cfg']['ThColor']              = '#000';
+// table data row background
+$GLOBALS['cfg']['BgOne']                = '#E5E5E5';
+// table data row background, alternate
+$GLOBALS['cfg']['BgTwo']                = '#D5D5D5';
